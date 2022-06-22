09afd158cf54b9e2ba1e1c9eb2fa116796a42088
Allan-Sartor <allansartor@outlook.com>
2022-06-22 17:44:44 -0300

    Fix: Remove React Query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,8 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
-import { ReactQueryDevtools } from 'react-query/devtools';
 import { theme } from '../styles/theme'
-
-import { QueryClientProvider } from 'react-query';
 import { SidebarDrawerProvider } from '../contexts/SidebarDrawerContext'
-// import { makeServer } from '../services/mirage'
-import { queryClient } from '../services/queryClient';
 import { AuthProvider } from '../contexts/AuthContext';
-import Head from 'next/head';
 
 if (process.env.NODE_ENV === 'development') {
   // makeServer();
@@ -17,20 +11,16 @@ if (process.env.NODE_ENV === 'development') {
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <title>Pontodesk.</title>
-      </Head>
-      <QueryClientProvider client={queryClient}>
-        <ChakraProvider resetCSS theme={theme}>
-          <AuthProvider>
-            <SidebarDrawerProvider>
-              <Component {...pageProps} />
-            </SidebarDrawerProvider>
-          </AuthProvider>
-        </ChakraProvider>
 
-        {/* <ReactQueryDevtools /> */}
-      </QueryClientProvider>
+      <ChakraProvider resetCSS theme={theme}>
+        <AuthProvider>
+          <SidebarDrawerProvider>
+            <Component {...pageProps} />
+          </SidebarDrawerProvider>
+        </AuthProvider>
+      </ChakraProvider>
+
+
     </>
 
   )

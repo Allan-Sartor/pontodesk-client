76cccd11e8c7a648a58705326b097e0165819149
Allan-Sartor <allansartor@outlook.com>
2022-07-01 17:38:31 -0300

    Fix: corrections of scroolbar styles

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -4,16 +4,32 @@ import { Header } from '../Header';
 import { Sidebar } from '../Sidebar';
 
 type CardProps = {
-  children: ReactElement;
+  children?: ReactElement;
 }
 
 export default function Card({ children }: CardProps) {
   return (
-    <Flex direction="column" height="100vh">
+    <Flex
+      direction="column"
+      height="100vh"
+      overflowY="auto"
+      css={{
+        '&::-webkit-scrollbar': {
+          width: '4px',
+        },
+        '&::-webkit-scrollbar-track': {
+          width: '6px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          background: 'gray',
+          borderRadius: '24px',
+        },
+      }}
+    >
       <Header />
       <Flex w="100%" maxWidth={1480} my="6" mx="auto" px="6">
         <Sidebar />
-        { children }
+        {children}
       </Flex>
     </Flex>
   )

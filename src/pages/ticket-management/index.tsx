45d6205934472fc5fbcf179b2818af704aa24d0f
Allan Sartor <allansartor@outlook.com>
2022-07-08 11:59:30 -0300

    Feature: Created new page for admin management

--- src/pages/ticket-management/index.tsx
+++ src/pages/ticket-management/index.tsx
@@ -24,9 +24,17 @@ export default function ticketList() {
     <Card>
       <CardBox title="Chamados em aberto">
         <>
-        <Title name="Pontodesk. | Chamados" />
+          <Title name="Pontodesk. | Chamados" />
 
-        <Table colorScheme={schemeColor}>
+          {
+            isLoading ? (
+              <Flex justify="center">
+                <Spinner />
+              </Flex>
+            ) : isError ? (
+              <Text>Não foi possível carregar os chamados!</Text>
+            ) : (
+              <Table colorScheme={schemeColor}>
                 <Thead>
                   <Tr>
                     <Th px={["4", "4", "6"]} color="gray.300" width="8">
@@ -40,105 +48,98 @@ export default function ticketList() {
                 </Thead>
 
                 <Tbody>
-                  {isLoading ? (
-                    <Flex justify="center">
-                      <Spinner />
-                    </Flex>
-                  ) : (
-                    <>
-                      {data.map(call => {
-                        return (
-                          <Tr key={call.id}>
-                            <Td px={["4", "4", "6"]}>
-                              <Checkbox colorScheme="green" />
-                            </Td>
+                  {data.map(call => {
+                    return (
+                      <Tr key={call.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="green" />
+                        </Td>
 
+                        <Td>
+                          <Box>
+                            <Link color="green.400">
+                              <Text fontWeight="bold">{call.title}</Text>
+                            </Link>
+                          </Box>
+                        </Td>
+
+                        {
+                          call.call_status === true
+                            ?
                             <Td>
-                              <Box>
-                                <Link color="green.400">
-                                  <Text fontWeight="bold">{call.title}</Text>
-                                </Link>
-                              </Box>
+                              <Text fontWeight="bold" color="yellow.400">Aberto</Text>
                             </Td>
-
-                            {
-                              call.call_status === true
-                                ?
-                                <Td>
-                                  <Text fontWeight="bold" color="yellow.400">Aberto</Text>
-                                </Td>
-                                :
-                                <Td>
-                                  <Text fontWeight="bold" color="green.400">Resolvido</Text>
-                                </Td>
-                            }
-
-                            {
-                              isWideVersion && <Td>
-                                {
-                                  new Intl.DateTimeFormat("pt-br")
-                                    .format(new Date(call.created_at))
-                                }
-                              </Td>
-                            }
-
+                            :
                             <Td>
-                              {
-                                isWideVersion && (
-                                  <Flex justify="center">
-                                    <Button
-                                      as="a"
-                                      size="sm"
-                                      cursor="pointer"
-                                      fontSize="sm"
-                                      colorScheme="green"
-                                      leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
-                                      variant='outline'
-                                    >
-                                      Editar
-                                    </Button>
-
-                                    <Button
-                                      as="a"
-                                      size="sm"
-                                      cursor="pointer"
-                                      fontSize="sm"
-                                      colorScheme="green"
-                                      leftIcon={<Icon as={RiEyeLine} fontSize="20" />}
-                                      variant='outline'
-                                    >
-                                      Visualizar
-                                    </Button>
-
-                                    <Button
-                                      as="a"
-                                      size="sm"
-                                      cursor="pointer"
-                                      fontSize="sm"
-                                      colorScheme="green"
-                                      leftIcon={<Icon as={RiCheckLine} fontSize="20" />}
-                                      variant='outline'
-                                    >
-                                      Resolver
-                                    </Button>
-                                  </Flex>
-                                )}
+                              <Text fontWeight="bold" color="green.400">Resolvido</Text>
                             </Td>
-                          </Tr>
-                        )
-                      })}
-                    </>
-                  )}
+                        }
 
+                        {
+                          isWideVersion && <Td>
+                            {
+                              new Intl.DateTimeFormat("pt-br")
+                                .format(new Date(call.created_at))
+                            }
+                          </Td>
+                        }
+
+                        <Td>
+                          {
+                            isWideVersion && (
+                              <Flex justify="center">
+                                <Button
+                                  as="a"
+                                  size="sm"
+                                  cursor="pointer"
+                                  fontSize="sm"
+                                  colorScheme="green"
+                                  leftIcon={<Icon as={RiPencilLine} fontSize="20" />}
+                                  variant='outline'
+                                >
+                                  Editar
+                                </Button>
+
+                                <Button
+                                  as="a"
+                                  size="sm"
+                                  cursor="pointer"
+                                  fontSize="sm"
+                                  colorScheme="green"
+                                  leftIcon={<Icon as={RiEyeLine} fontSize="20" />}
+                                  variant='outline'
+                                >
+                                  Visualizar
+                                </Button>
+
+                                <Button
+                                  as="a"
+                                  size="sm"
+                                  cursor="pointer"
+                                  fontSize="sm"
+                                  colorScheme="green"
+                                  leftIcon={<Icon as={RiCheckLine} fontSize="20" />}
+                                  variant='outline'
+                                >
+                                  Resolver
+                                </Button>
+                              </Flex>
+                            )}
+                        </Td>
+                      </Tr>
+                    )
+                  })}
                 </Tbody>
               </Table>
 
-              {/* <Pagination 
-                  totalCountOfRegisters={pagination.meta.totalItems}
-                  currentPage={pagination.meta.current_page}
-                  registerPerPage={pagination.meta.itemsPerPage}
-                  onPageChange={setPage}
-                /> */}
+              //  <Pagination 
+              //     totalCountOfRegisters={pagination.meta.totalItems}
+              //     currentPage={pagination.meta.current_page}
+              //     registerPerPage={pagination.meta.itemsPerPage}
+              //     onPageChange={setPage}
+              //   />
+            )
+          }
         </>
       </CardBox>
     </Card>
